3b1e17818f577061ea16a3ac3ac1e663ec87c12e
Mikolaj Szabo <savingprivateblue@gmail.com>
2018-04-03 23:58:37 +0200

    Change log message format

--- src/Lib.hs
+++ src/Lib.hs
@@ -24,12 +24,20 @@ agent :: Process ()
 agent = do
   Start pids seed <- expect
   myPid           <- getSelfPid
-  log myPid "started"
+  logInfo $ (show myPid) ++ " Started"
   rngPid          <- spawnLocal $ rng myPid $ mkStdGen seed
   listen myPid pids rngPid [] 0
-  log myPid "done"
+  logInfo "Done"
   where
-    log pid msg = logInfo $ (show pid) ++ ": " ++ msg
+    rng :: ProcessId -> StdGen -> Process ()
+    rng agentPid gen = do
+      maybeStop <- expectTimeout 1000000 :: Process (Maybe Protocol)
+      case maybeStop of
+        Just Stop -> pure ()
+        Nothing -> do
+          let (r, gen') = randomR (0, 1) gen
+          send agentPid (Gen r)
+          rng agentPid gen'
 
     listen :: ProcessId
            -> [ProcessId]
@@ -42,41 +50,30 @@ agent = do
       case msg of
         Gen d -> do
           let nts = ts + 1
-          log myPid $ "sending " ++ (show d) ++ " at " ++ (show nts)
+          logInfo $ (show nts) ++ ". Sending " ++ (show d)
           broadcast pids (Msg d nts myPid)
           listen myPid pids rngPid ((d, nts, myPid) : history) nts
         Msg d mts fromPid | fromPid == myPid -> do
-          log myPid "ignoring own message"
+          logInfo "Ignoring own message"
           listen myPid pids rngPid history ts
         Msg d mts fromPid -> do
-          log myPid $ "received " ++ (show d) ++ " at " ++ (show mts)
           let nts = if (mts >= ts) then mts + 1 else ts + 1
+          logInfo $ (show nts) ++ ". Received " ++ (show d) ++ " sent by " ++ (show fromPid) ++ " at " ++ (show mts)
           listen myPid pids rngPid ((d, mts, fromPid) : history) nts
         Stop -> do
-          log myPid "stopping RNG"
+          logInfo "Stopping RNG"
           send rngPid Stop
           listen myPid pids rngPid history ts
         Kill -> do
-          logInfo "\nRESULT: "
-          logInfo $ show $ result $ sortOn (\(_,t,_) -> t) history
-          logInfo ""
-
-    rng :: ProcessId -> StdGen -> Process ()
-    rng agentPid gen = do
-      maybeStop <- expectTimeout 1000 :: Process (Maybe Protocol)
-      case maybeStop of
-        Just Stop -> pure ()
-        Nothing -> do
-          let (r, gen') = randomR (0, 1) gen
-          send agentPid (Gen r)
-          rng agentPid gen'
+          let res = result $ sortOn (\(_,t,_) -> t) history
+          logInfo $ "\nRESULT: " ++ (show res) ++ "\n"
 
     result :: [(Double, Int, ProcessId)] -> (Int, Double)
     result history =
       let l       = length history
           values  = map (\(v,_,_) -> v) history
           indices = map fromIntegral [1..l]
-          summa = sum $ zipWith (*) indices values
+          summa   = sum $ zipWith (*) indices values
       in (l, summa)
 
 remotable ['agent]
